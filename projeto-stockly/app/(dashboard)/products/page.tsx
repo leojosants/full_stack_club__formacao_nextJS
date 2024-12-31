c1163d756eb4baf439f8ea7f4afb78ae0b7f98fa
Leonardo Santos <leonardojosantos@gmail.com>
2024-12-31 18:29:12 -0300

    Módulo 05 - Data Fetching & Caching -- Data Fetching & Listagem de Produtos (REFAZENDO)

--- projeto-stockly/app/(dashboard)/products/page.tsx
+++ projeto-stockly/app/(dashboard)/products/page.tsx
@@ -1,5 +1,33 @@
-export default function ProductsPage() {
+import { productTableColumns } from "./_components/table-columns";
+import { DataTable } from "@/app/_components/ui/data-table";
+import { Button } from "@/app/_components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { db } from "@/app/_lib/prisma";
+
+
+export default async function ProductsPage() {
+    const products = await db.product.findMany({});
+
     return (
-        <h1>ProductsPage</h1>
+        <div className="m-8 w-full space-y-8 bg-white p-8 rounded-lg">
+            <div className="flex w-full items-center justify-between">
+                <div className="space-y-1">
+                    <span className="text-xs font-semibold text-slate-500">
+                        Gestão de Produtos
+                    </span>
+
+                    <h2 className="text-xl font-semibold">
+                        Produtos
+                    </h2>
+                </div>
+
+                <Button className="gap-2">
+                    <PlusIcon size={20} />
+                    Novo produto
+                </Button>
+            </div>
+
+            <DataTable columns={productTableColumns} data={products} />
+        </div>
     );
 };

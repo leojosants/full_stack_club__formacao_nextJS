fbc4d06ef3aebd8913a5b9f5b17f726cfe913d7e
Leonardo Santos <leonardojosantos@gmail.com>
2024-12-31 20:55:48 -0300

    Módulo 05 - Data Fetching & Caching -- Route Handlers (REFAZENDO)

---
+++ projeto-stockly/app/api/products/route.ts
@@ -0,0 +1,39 @@
+/*
+APENAS PARA REFERENCIA
+*/
+
+import { db } from "@/app/_lib/prisma";
+
+
+interface HttpStatusCodeInterface {
+    created: number;
+    ok: number;
+};
+
+const httpStatusCode: HttpStatusCodeInterface = {
+    created: 201,
+    ok: 200,
+};
+
+export async function GET() {
+    const products = await db.product.findMany({});
+
+    return Response.json(
+        products, { status: httpStatusCode.ok }
+    );
+};
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const name = body.name;
+    const price = body.price;
+    const stock = body.stock;
+
+    await db.product.create(
+        { data: { name, price, stock } }
+    );
+
+    return Response.json(
+        {}, { status: httpStatusCode.created }
+    );
+};

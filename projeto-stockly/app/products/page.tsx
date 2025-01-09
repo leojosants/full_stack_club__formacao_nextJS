353dec679015d17c6505ebf01d01ec30356b5045
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-09 11:25:42 -0300

    Revisando -- app/products/page.tsx --> ok

--- projeto-stockly/app/products/page.tsx
+++ projeto-stockly/app/products/page.tsx
@@ -1,25 +1,16 @@
 import { Header, HeaderLeft, HeaderRight, HeaderTitle } from "../_components/header";
 import { CreateProductButton } from "./_components/create-product-button";
+import { getProducts } from "../_data-access/product/get-products";
 import { productTableColumns } from "./_components/table-columns";
 import { DataTable } from "../_components/ui/data-table";
 import { HeaderSubtitle } from '../_components/header';
 
 
 const ProductsPage = async () => {
-    const productsResponse = await fetch(
-        "http://localhost:3000/api/products",
-        {
-            next: {
-                revalidate: 60,
-                tags: ["get-products"],
-            }
-        }
-    );
-
-    const products = await productsResponse.json();
+    const products = await getProducts();
 
     return (
-        <div className="m-8 w-full space-y-8 bg-white p-8 rounded-lg overflow-auto">
+        <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
             <Header>
                 <HeaderLeft>
                     <HeaderSubtitle>

a3ca4601a506778cbf403319b6cf9ad3deabcd39
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-09 14:24:39 -0300

    Revisando -- app/sales/page.tsx --> ok

--- projeto-stockly/app/sales/page.tsx
+++ projeto-stockly/app/sales/page.tsx
@@ -8,6 +8,10 @@ import { DataTable } from "../_components/ui/data-table";
 import { HeaderSubtitle } from '../_components/header';
 
 
+// Essa página será montada uma vez e reutilizada (SSG), podendo ser incrementada de forma regenerativa (ISR)
+export const dynamic = "force-static";
+export const revalidate = 10;
+
 export const SalesPage = async () => {
     const sales = await getSales();
     const products = await getProducts();
@@ -28,12 +32,13 @@ export const SalesPage = async () => {
     );
 
     return (
-        <div className="m-8 w-full space-y-8 bg-white p-8 rounded-lg overflow-auto">
+        <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
             <Header>
                 <HeaderLeft>
                     <HeaderSubtitle>
                         {"Gestão de Vendas"}
                     </HeaderSubtitle>
+
                     <HeaderTitle>
                         {"Vendas"} ({sales.length})
                     </HeaderTitle>

0ccfd236c52608032e6c89861fec37acfcd1fe1a
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-09 10:37:53 -0300

    Revisando -- app/_helpers/currency.ts --> ok

--- projeto-stockly/app/_helpers/currency.ts
+++ projeto-stockly/app/_helpers/currency.ts
@@ -1,5 +1,5 @@
 export const formatCurrency = (value: number) => {
-    return new Intl.NumberFormat(
-        "pt-BR", { style: "currency", currency: "BRL" }
-    ).format(value);
+    return new Intl
+        .NumberFormat("pt-BR", { style: "currency", currency: "BRL" })
+        .format(value);
 };

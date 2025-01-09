8e6884e3955e03dd7ab10dcf4fbf8680baec3b66
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-09 10:52:45 -0300

    Revisando -- app/(dashboard)/_components/most-sold-products.tsx --> ok

--- projeto-stockly/app/(dashboard)/_components/most-sold-products.tsx
+++ projeto-stockly/app/(dashboard)/_components/most-sold-products.tsx
@@ -9,7 +9,7 @@ export const MostSoldProducts = async () => {
     return (
         <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
             <p className="p-6 text-lg font-semibold text-slate-900">
-                Produtos mais vendidos
+                {"Produtos mais vendidos"}
             </p>
 
             <div className="space-y-7 overflow-y-auto px-6 pb-6">

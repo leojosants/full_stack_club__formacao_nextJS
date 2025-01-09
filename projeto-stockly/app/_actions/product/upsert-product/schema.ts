8fa8d8296a5ddf067c6bc17f7feec39965f3419b
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-08 23:36:59 -0300

    Revisando -- app/_actions/product/upsert-product/schema.ts --> ok

--- projeto-stockly/app/_actions/product/upsert-product/schema.ts
+++ projeto-stockly/app/_actions/product/upsert-product/schema.ts
@@ -6,7 +6,7 @@ export const upsertProductSchema = z.object(
         id: z.string().uuid().optional(),
 
         name: z.string().trim()
-            .min(2, { message: "O nome do produto é obrigatório." }),
+            .min(1, { message: "O nome do produto é obrigatório." }),
 
         price: z.number()
             .min(0.01, { message: "O preço do produto é obrigatório." }),

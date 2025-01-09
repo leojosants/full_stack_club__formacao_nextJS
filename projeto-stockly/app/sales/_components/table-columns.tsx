752511bac15a45295e9689b3a88283cd582f580b
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-09 12:07:58 -0300

    Revisando -- app/products/_components/table-columns.tsx --> ok

--- projeto-stockly/app/sales/_components/table-columns.tsx
+++ projeto-stockly/app/sales/_components/table-columns.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { ProductDTO } from "@/app/_data-access/product/get-products";
+import { SalesTableDropdownMenu } from "./table-dropdown-menu";
 import { ComboboxOption } from "@/app/_components/ui/combobox";
 import { SaleDTO } from "@/app/_data-access/sale/get-sale";
 import { formatCurrency } from "@/app/_helpers/currency";
 import { ColumnDef } from "@tanstack/react-table";
-import { SalesTableDropdownMenu } from "./table-dropdown-menu";
 
 
 interface SaleTableColumns extends SaleDTO {

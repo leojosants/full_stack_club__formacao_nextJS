6bfe505c51ef4d466cd74f63c54b3da98beb375c
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-19 17:51:54 -0300

    Módulo 09 - Sistema de Reserva de Barbearias -- Reserva de um barbeiro (pausado no tempo de vídeo 1:28:00min)

--- projeto-barber/app/_server-actions/create-booking/create-booking.ts
+++ projeto-barber/app/_server-actions/create-booking/create-booking.ts
@@ -3,12 +3,20 @@
 import { createBookingRevalidatePath } from "./create-booking-revalidate-path";
 import { CreateBookingParams } from "./create-booking-params";
 
-import { revalidatePath } from "next/cache";
-
+import { authOptions } from "@/app/_lib/auth";
 import { db } from "@/app/_lib/prisma";
 
+import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
 
-export const createBooking = async (params: CreateBookingParams) => {
+
+export const createBooking = async (params: CreateBookingParams): Promise<void> => {
+    const user = await getServerSession(authOptions);
+
+    if (!user) {
+        throw new Error("Usuário não autenticado!");
+    }
+
     await db.booking.create(
         { data: params }
     );

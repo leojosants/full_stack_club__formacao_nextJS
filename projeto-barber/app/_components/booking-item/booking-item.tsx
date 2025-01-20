c278ca7b4ea8161c4fcba324d5c636d25ade36f7
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-20 18:25:53 -0300

    Módulo 09 - Sistema de Reserva de Barbearias -- Reserva de um barbeiro (pausado no tempo de video 37min)

--- projeto-barber/app/_components/booking-item/booking-item.tsx
+++ projeto-barber/app/_components/booking-item/booking-item.tsx
@@ -1,11 +1,14 @@
-import { BookingItemProps } from "./booking-item-props";
-
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Card, CardContent } from "../ui/card";
 import { Badge } from "../ui/badge";
 
+import { BookingItemProps } from "./booking-item-props";
+
 import { format, isFuture } from "date-fns";
 import { ptBR } from "date-fns/locale";
+
+import Image from "next/image";
 
 
 export const BookingItem = (props: BookingItemProps): JSX.Element => {
@@ -13,58 +16,77 @@ export const BookingItem = (props: BookingItemProps): JSX.Element => {
     const isConfirmed = isFuture(booking.date);
 
     return (
-        <>
-            <Card className={"min-w-[90%] hover:text-gray-400 transition-all duration-300 ease-in-out"}>
-                <CardContent className={"flex justify-between p-0"}>
-                    <div className="flex flex-col gap-2 py-5 pl-5">
-                        <Badge className={"w-fit"} variant={isConfirmed ? "default" : "secondary"}>
-                            {isConfirmed ? "Confirmado" : "Finalizado"}
-                        </Badge>
+        <Sheet>
+            <SheetTrigger className={"w-full"}>
+                <Card className={"min-w-[90%] hover:text-gray-400 transition-all duration-300 ease-in-out"}>
+                    <CardContent className={"flex justify-between p-0"}>
+                        <div className="flex flex-col gap-2 py-5 pl-5">
+                            <Badge className={"w-fit"} variant={isConfirmed ? "default" : "secondary"}>
+                                {isConfirmed ? "Confirmado" : "Finalizado"}
+                            </Badge>
 
-                        <h3 className="font-semibold">
-                            {booking.service.name}
-                        </h3>
+                            <h3 className="font-semibold">
+                                {booking.service.name}
+                            </h3>
 
-                        <div className="flex items-center gap-2">
-                            <Avatar className={"h-6 w-6"}>
-                                <AvatarImage
-                                    src={booking.service.barbershop.imageUrl}
-                                />
-                            </Avatar>
+                            <div className="flex items-center gap-2">
+                                <Avatar className={"h-6 w-6"}>
+                                    <AvatarImage
+                                        src={booking.service.barbershop.imageUrl}
+                                    />
+                                </Avatar>
+
+                                <p className="text-sm">
+                                    {booking.service.barbershop.name}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+                            <p className="text-sm capitalize">
+                                {
+                                    format(
+                                        booking.date, "MMMM", { locale: ptBR }
+                                    )
+                                }
+                            </p>
+
+                            <p className="text-2xl">
+                                {
+                                    format(
+                                        booking.date, "dd", { locale: ptBR }
+                                    )
+                                }
+                            </p>
 
                             <p className="text-sm">
-                                {booking.service.barbershop.name}
+                                {
+                                    format(
+                                        booking.date, "HH:mm", { locale: ptBR }
+                                    )
+                                }
                             </p>
                         </div>
-                    </div>
+                    </CardContent>
+                </Card>
+            </SheetTrigger>
 
-                    <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-                        <p className="text-sm capitalize">
-                            {
-                                format(
-                                    booking.date, "MMMM", { locale: ptBR }
-                                )
-                            }
-                        </p>
+            <SheetContent className={"w-[90%]"}>
+                <SheetHeader>
+                    <SheetTitle className={"text-left"}>
+                        {"Informações da Reserva"}
+                    </SheetTitle>
+                </SheetHeader>
 
-                        <p className="text-2xl">
-                            {
-                                format(
-                                    booking.date, "dd", { locale: ptBR }
-                                )
-                            }
-                        </p>
-
-                        <p className="text-sm">
-                            {
-                                format(
-                                    booking.date, "HH:mm", { locale: ptBR }
-                                )
-                            }
-                        </p>
-                    </div>
-                </CardContent>
-            </Card>
-        </>
+                <div className="relative h-[180px] w-full">
+                    <Image
+                        alt={`Mapa da barbearia ${booking.service.barbershop.name}`}
+                        className={"object-cover"}
+                        src={"/map.png"}
+                        fill
+                    />
+                </div>
+            </SheetContent>
+        </Sheet>
     );
 };

8c6849b88b51e08fa185a8fa0efd34c958dcfee0
Leonardo Santos <leonardojosantos@gmail.com>
2025-01-23 11:57:14 -0300

    Módulo 10 - Novidades do Nextjs 15 -- exemplo Executing code after a response with unstable_after (Experimental)

--- modulo_10__novidades_do_nextjs_15/nextjs-15/app/counter/page.tsx
+++ modulo_10__novidades_do_nextjs_15/nextjs-15/app/counter/page.tsx
@@ -1,7 +1,21 @@
+import { after } from "next/server";
+
 import { JSX } from "react";
 
 
+export const dynamic = "force-dynamic";
+
 const Counter = (): JSX.Element => {
+    after(
+        async () => {
+            await new Promise(
+                (resolve) => setTimeout(resolve, 5000)
+            );
+
+            console.log("after is running");
+        }
+    );
+
     return (
         <div>
             {`Random: ${Math.random()}`}
